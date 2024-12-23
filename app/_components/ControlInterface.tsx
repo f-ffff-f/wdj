b1787b62dbea893783f560f62a296e09f9ea469e
softwat <ghr95223@gmail.com>
2024-12-23 20:09:25 +0900

    feat: gesture for chorme policy

--- app/_components/ControlInterface.tsx
+++ app/_components/ControlInterface.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Deck from '@/app/_components/Deck'
 import Crossfader from '@/app/_components/CrossFader'
 import { DECK_IDS } from '@/app/_lib/constants'
 
 const ControlInterface = () => {
+    const [isGestured, setIsGestured] = useState(false)
     return (
         <div>
-            <div className="flex flex-wrap justify-center gap-8 mb-8">
-                {DECK_IDS.map((_id) => (
-                    <Deck id={_id} key={_id} />
-                ))}
-            </div>
-            <div className="flex justify-center">
-                <Crossfader />
-            </div>
+            {!isGestured ? (
+                <button onClick={() => setIsGestured(true)}>gesture</button>
+            ) : (
+                <>
+                    <div className="flex flex-wrap justify-center gap-8 mb-8">
+                        {DECK_IDS.map((_id) => (
+                            <Deck id={_id} key={_id} />
+                        ))}
+                    </div>
+                    <div className="flex justify-center">
+                        <Crossfader />
+                    </div>
+                </>
+            )}
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 'use client'
 
 import ControlInterface from '@/app/_components/ControlInterface'
-import React, { useState } from 'react'
 
 const Home: React.FC = () => {
-    const [isGestured, setIsGestured] = useState(false)
-    return <>{isGestured ? <ControlInterface /> : <button onClick={() => setIsGestured(true)}>gesture</button>}</>
+    return <ControlInterface />
 }
 
 export default Home
