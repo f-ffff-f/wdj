cc61028ebee31666c1daaca55cb584a716056ca8
softwat <ghr95223@gmail.com>
2025-01-26 19:53:30 +0900

    fix: minor issues

--- app/_lib/state/index.ts
+++ app/_lib/state/index.ts
@@ -58,7 +58,7 @@ export const addTrackToLibrary = async (file: File) => {
     const track: ITrack = {
         id: trackId,
         fileName: file.name,
-        playlistIds: [state.vault.currentPlaylistId],
+        playlistIds: state.vault.currentPlaylistId ? [state.vault.currentPlaylistId] : [],
     }
 
     state.vault.tracks.push({ ...track, url: URL.createObjectURL(file) })
