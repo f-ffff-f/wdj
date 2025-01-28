2bcfb63e01b692accfabcec3192cc089f05012e6
softwat <ghr95223@gmail.com>
2025-01-28 21:41:19 +0900

    feat: reusable getUserIdFromToken

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,3 +1,27 @@
+import jwt from 'jsonwebtoken'
+import { NextResponse } from 'next/server'
+
+export const getUserIdFromToken = (request: Request) => {
+    // Authorization 헤더 추출
+    const authHeader = request.headers.get('Authorization')
+    if (!authHeader) {
+        return NextResponse.json({ error: 'Need Authorization' }, { status: 401 })
+    }
+
+    // Bearer 토큰에서 실제 토큰 값 추출
+    const token = authHeader.split(' ')[1]
+    if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined')
+    }
+
+    // JWT 토큰 검증 및 디코딩
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
+        userId: string
+    }
+
+    return decoded
+}
+
 export const formatTimeUI = (seconds: number): string => {
     const min = Math.floor(seconds / 60)
     const sec = Math.floor(seconds % 60)
--- app/api/user/me/route.ts
+++ app/api/user/me/route.ts
@@ -3,6 +3,7 @@ import { prisma } from '@/lib/prisma'
 import jwt from 'jsonwebtoken'
 import { UserDTO } from '@/types/dto'
 import { UserMeAPI } from '@/types/api'
+import { getUserIdFromToken } from '@/app/_lib/utils'
 
 /**
  * 인증된 사용자의 정보를 반환하는 API 엔드포인트
@@ -11,26 +12,15 @@ import { UserMeAPI } from '@/types/api'
 
 export async function GET(request: Request) {
     try {
-        // Authorization 헤더 추출
-        const authHeader = request.headers.get('Authorization')
-        if (!authHeader) {
-            return NextResponse.json({ error: 'Need Authorization' }, { status: 401 })
-        }
+        const result = getUserIdFromToken(request)
 
-        // Bearer 토큰에서 실제 토큰 값 추출
-        const token = authHeader.split(' ')[1]
-        if (!process.env.JWT_SECRET) {
-            throw new Error('JWT_SECRET is not defined')
-        }
-
-        // JWT 토큰 검증 및 디코딩
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
-            userId: string
+        if (result instanceof NextResponse) {
+            return result
         }
 
         // 디코딩된 userId로 사용자 정보 조회
         const user: UserDTO | null = await prisma.user.findUnique({
-            where: { id: decoded.userId },
+            where: { id: result.userId },
             select: {
                 id: true,
                 email: true,
