946b92b8b06ba5b2a645db9bf6f1a2bf5e62c782
softwat <ghr95223@gmail.com>
2025-01-28 21:09:15 +0900

    feat: add dto playlist

--- types/dto.ts
+++ types/dto.ts
@@ -3,3 +3,7 @@ import { Prisma } from '@prisma/client'
 export type UserDTO = Prisma.UserGetPayload<{
     select: { id: true; email: true; createdAt: true }
 }>
+
+export type PlaylistDTO = Prisma.PlaylistGetPayload<{
+    select: { id: true; name: true; createdAt: true }
+}>
