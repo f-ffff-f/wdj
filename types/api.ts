94c8541ac54917888a3e52fb5754e6384488349e
softwat <ghr95223@gmail.com>
2025-01-28 20:58:13 +0900

    feat: add model Playlist

--- types/api.ts
+++ types/api.ts
@@ -1,57 +1,10 @@
-import { PlaylistDTO, UserDTO } from './dto'
-
-type APIError = {
-    error: string
-}
-
-// 제네릭 API 응답 타입 생성
-type ApiResponse<T> = T | APIError
+import { UserDTO } from './dto'
 
 // api/user/me
-export type UserMeAPI = {
-    Response: ApiResponse<UserDTO>
-}
+export type UserMeAPI = { Response: UserDTO }
 
 // api/user/login
 export type UserLoginAPI = {
     Request: { email: string; password: string }
-    Response: ApiResponse<{ message: string; token: string } & UserDTO>
-}
-
-// api/playlist/create
-export type CreatePlaylistAPI = {
-    Request: {
-        name: string
-    }
-    Response: ApiResponse<PlaylistDTO>
-}
-
-// api/playlist/read
-export type GetPlaylistAPI = {
-    Request: {
-        id: string
-    }
-    Response: ApiResponse<PlaylistDTO>
-}
-
-// api/playlist/read-all
-export type GetPlaylistsAPI = {
-    Response: ApiResponse<PlaylistDTO[]>
-}
-
-// api/playlist/update
-export type UpdatePlaylistAPI = {
-    Request: {
-        id: string
-        name?: string
-    }
-    Response: ApiResponse<PlaylistDTO>
-}
-
-// api/playlist/delete
-export type DeletePlaylistAPI = {
-    Request: {
-        id: string
-    }
-    Response: ApiResponse<{ message: string; id: string }>
+    Response: { message: string; token: string } & UserDTO
 }
--- types/dto.ts
+++ types/dto.ts
@@ -3,19 +3,3 @@ import { Prisma } from '@prisma/client'
 export type UserDTO = Prisma.UserGetPayload<{
     select: { id: true; email: true; createdAt: true }
 }>
-
-export type PlaylistDTO = Prisma.PlaylistGetPayload<{
-    select: {
-        id: true
-        name: true
-        createdAt: true
-        updatedAt: true
-        userId: true
-        user: {
-            select: {
-                id: true
-                email: true
-            }
-        }
-    }
-}>
