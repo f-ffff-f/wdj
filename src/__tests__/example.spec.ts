aa4e72f77cd5ac119ee378e61093196efe7609de
softwat <ghr95223@gmail.com>
2025-02-06 22:02:39 +0900

    test: auth-fallback

--- src/__tests__/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('Google 검색 페이지 테스트', async ({ page }) => {
-    await page.goto('https://www.google.com')
-    await expect(page).toHaveTitle(/Google/)
-})
