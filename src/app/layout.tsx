64e9c7bf2cd7bb517b213d7f380a57e78f27186e
softwat <ghr95223@gmail.com>
2025-02-11 11:53:31 +0900

    deploy: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { EnvironmentIndicator } from '@/components/Debugger/EnvironmentIndicator'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -17,6 +18,7 @@ export default function RootLayout({
             <body className={`antialiased`}>
                 <EnvironmentIndicator />
                 {children}
+                <Analytics />
             </body>
         </html>
     )
