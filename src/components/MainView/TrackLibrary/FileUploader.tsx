148802a54b0f5449ed0f255ac9e8b0509cb2ce1a
f-ffff-f <ghr95223@gmail.com>
2025-04-08 14:45:32 +0900

    ux: remove file input loader

--- src/components/MainView/TrackLibrary/FileUploader.tsx
+++ src/components/MainView/TrackLibrary/FileUploader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { InputFile } from '@/components/ui/inputFile'
 import { useTrackMutation } from '@/lib/client/hooks/useTrackMutaion'
-import MyLoaderIcon from '@/components/ui/myLoaderIcon'
+
 const FileUploader = () => {
     const { createTrackMutation } = useTrackMutation()
     const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,13 +14,13 @@ const FileUploader = () => {
     }
     return (
         <div>
-            {createTrackMutation.isPending ? (
-                <div className="flex items-center justify-center">
-                    <MyLoaderIcon />
-                </div>
-            ) : (
-                <InputFile type="file" accept="audio/mp3" onChange={(e) => handleFileUpload(e)} id="file-uploader" />
-            )}
+            <InputFile
+                type="file"
+                accept="audio/mp3"
+                onChange={(e) => handleFileUpload(e)}
+                id="file-uploader"
+                disabled={createTrackMutation.isPending}
+            />
         </div>
     )
 }
