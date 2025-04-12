72e83001f979fbe52e5f093f2e148a25dea19c0e
f-ffff-f <ghr95223@gmail.com>
2025-04-13 01:54:19 +0900

    chore: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,15 +10,15 @@ export default defineConfig({
         command: 'npm run build && npm run start',
         port: 3000,
 
-        // 20250410 reuseExistingServer가 true로 돼있던것이 CI 환경 런타임에서 버그가 계속 발생했던 원인인것 같다
-        reuseExistingServer: !process.env.CI,
-        //
+        reuseExistingServer: false,
     },
     use: {
-        headless: true,
+        headless: false,
         baseURL: 'http://localhost:3000',
         screenshot: 'only-on-failure',
         video: 'on',
         trace: 'on-first-retry',
     },
+    workers: 1,
+    retries: 10,
 })
