963fe09b1b310b5c6c75a5d6e3f998e780468249
f-ffff-f <ghr95223@gmail.com>
2025-05-08 00:33:36 +0900

    hotfix: auth.js critical issue

--- src/app/SessionProvider.tsx
+++ src/app/SessionProvider.tsx
@@ -1,11 +1,30 @@
 'use client'
 
+import { Session } from 'next-auth'
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react'
-import { ReactNode } from 'react'
+import { ReactNode, useMemo } from 'react'
 
 /**
  * Session provider that wraps the application to provide Auth.js v5 session context
  */
-export const SessionProvider = ({ children }: { children: ReactNode }) => {
-    return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+export const SessionProvider = ({
+    children,
+    session,
+    sessionKey,
+}: {
+    children: ReactNode
+    session: Session | null
+    sessionKey: number
+}) => {
+    const memoizedSessionKey = useMemo(() => {
+        console.log('session changed >>> ', session)
+
+        return sessionKey
+    }, [session, sessionKey])
+
+    return (
+        <NextAuthSessionProvider session={session} key={memoizedSessionKey}>
+            {children}
+        </NextAuthSessionProvider>
+    )
 }
